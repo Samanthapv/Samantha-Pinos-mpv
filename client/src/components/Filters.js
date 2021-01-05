8eb541d5be1b76e61b7a2402640ae1732a8fde63
samanthapinos <spinosv@gmail.com>
2021-01-05 12:59:28 +0100

    improved nav bar

--- client/src/components/Filters.js
+++ client/src/components/Filters.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import ItemGrid from "./ItemGrid";
 import NotFound from "./NotFound";
-import { Link, useHistory, useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 export default function CategoryNav(props) {
   let [colors, setColors] = useState([]);
@@ -14,6 +14,8 @@ export default function CategoryNav(props) {
   let [notFound, setNotFound] = useState(false);
   let location = useLocation();
   const history = useHistory();
+
+  //fetch colors and categories, parse queries from url and make them the state
 
   useEffect(() => {
     getColors();
@@ -42,6 +44,8 @@ export default function CategoryNav(props) {
       });
   };
 
+  //push filter params to the url
+
   useEffect(() => {
     filterItems(color, category);
 
@@ -51,6 +55,8 @@ export default function CategoryNav(props) {
 
     history.push(`/filter?${queryParams}`);
   }, [filters]);
+
+  //fetch the filtered items so they can be displayed
 
   const filterItems = (color, category) => {
     let url = `items/`;
@@ -65,11 +71,15 @@ export default function CategoryNav(props) {
       });
   };
 
+  //handle filter clicks
+
   const handleClick = (name, content) => {
     setNotFound(false);
     setFilters({ ...filters, [name]: content });
     name === "color" ? setColor(content) : setCategory(content);
   };
+
+  //reset filters and url
 
   const resetFilter = () => {
     setColor("");
@@ -87,7 +97,7 @@ export default function CategoryNav(props) {
             {categories.map(item => (
               <li
                 className={` categories link ${
-                  category === item.id ? "green" : "inactive"
+                  category === item.id ? "font-weight-bold" : "inactive"
                 }`}
                 key={item.id}
                 onClick={() => handleClick("category", item.id)}
@@ -97,9 +107,9 @@ export default function CategoryNav(props) {
             ))}
           </ul>
 
-          <div className="buttons mb-3 ">
+          <div className="buttons mb-3 ml-5">
             <select
-              className="w-64 border shadow-sm p-2 mr-3 rounded-full focus:outline-none"
+              className="w-64 border shadow-sm p-1 mr-3 rounded-full focus:outline-none"
               name="color"
               value={color}
               onChange={e => handleClick(e.target.name, e.target.value)}
